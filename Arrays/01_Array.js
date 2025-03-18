bdb211d5c83d9836cb7d3dbca593daf94a48913b
Priya-Panchal1099 <panchalkisu1099@gmail.com>
2025-03-18 11:36:47 +0530

    Basic Concept of JS

--- Arrays/01_Array.js
+++ Arrays/01_Array.js
@@ -1,1 +1,27 @@
 const arr = [0, 1, 2, 3, 4, 5];
+const heros=["shaktiman","naagraj"];
+
+const arr2 = [0, 1, 2, 3, 4];
+// console.log(arr[0]);
+
+// arr.push(6);
+// arr.pop();
+// arr.unshift(8);
+// arr.shift();
+// console.log(arr.includes(4));
+// console.log(arr.indexOf(5));
+// console.log(arr);
+
+const slArr=arr.slice(1,4);
+console.log(slArr);
+console.log("Real Array after slice:",arr);
+
+
+const spArr=arr.splice(1,4);
+console.log(spArr);
+console.log("Real Arr after splice:",arr);
+
+
+
+
+
